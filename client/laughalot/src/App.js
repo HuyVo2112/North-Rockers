a724fa9c225f5ab0f9781b5596e75a8295401f2e
IsmaelSTEM <113748633+TheCodingWizard0@users.noreply.github.com>
2022-09-17 17:40:46 -0400

    First complete draft of home page

--- client/laughalot/src/App.js
+++ client/laughalot/src/App.js
@@ -1,30 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar w/ text</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Login</a>
-            </li>
-          </ul>
+    <html>
+      <link rel="stylesheet" href='App.css'></link>
+      <link rel="stylesheet" href='index.css'></link>
+      <div className="App">
+        <header>
+          {/* <link rel="preconnect" href="https://fonts.googleapis.com"> 
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+          <link href="https://fonts.googleapis.com/css2?family=Acme&display=swap" rel="stylesheet"> */}
+            <nav class="navbar navbar-expand-lg navbar-dark p-4 bg-danger">
+            <a class="navbar-brand pl-3" href="#">Home</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarText">
+              <ul class="navbar-nav mr-auto">
+                {/* <li class="nav-item active">
+                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li> */}
+                <li class="nav-item">
+                  <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Login</a>
+                </li>
+              </ul>
+            
+            </div>
+          </nav>
+        </header>
+        <body class='colorClass'>
+                
          
+          
+        <div class="container h-100 colorClass">
+          <div class="row bigPadding">
+            <div class="col-sm">
+              <h2>LEADERBOARDS</h2>
+              <ol>
+                <li>Arihan</li>
+                <li>Dilreet</li>
+                <li>Huy</li>
+                <li>Ismael</li>
+              </ol>
+            </div>
+            <div class="col-sm">
+              <h2 class ="pb-5">LAUGHALOT</h2>
+              <button type="button" class="btn btn-lg  btn-danger">New Streak</button>
+            </div>
+            <div class="col-sm">
+              <h2>ADD FRIEND</h2>
+              <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
         </div>
-      </nav>
-    </div>
+        </body>
+      </div>
+    </html>
   );
 }
 
